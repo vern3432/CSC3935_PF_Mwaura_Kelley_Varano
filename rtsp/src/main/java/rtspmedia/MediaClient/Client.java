cc4719de183d38085c2a6d1924f9941cb1aaf6ee
Aidan <varanoab@merrimack.edu>
2024-04-26 14:52:12 -0400

    Added Jmenu for Client Gui

--- rtsp/src/main/java/rtspmedia/MediaClient/Client.java
+++ rtsp/src/main/java/rtspmedia/MediaClient/Client.java
@@ -4,7 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.FilenameFilter;
-
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 public class Client {
     JFrame frame;
     JButton setupButton, playButton, pauseButton, tearButton, descButton;
@@ -58,6 +59,38 @@ public class Client {
             scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
             mainPanel.add(scrollPane);
         }
+
+        // Set the entire GUI background to black
+        frame.getContentPane().setBackground(Color.BLACK);
+        mainPanel.setBackground(Color.BLACK);
+        buttonPanel.setBackground(Color.BLACK);
+        imagePanel.setBackground(Color.BLACK);
+
+        // Create menu bar and add to frame
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Options");
+        JMenuItem toggleTheme = new JMenuItem("Toggle Theme");
+        menu.add(toggleTheme);
+        menuBar.add(menu);
+        frame.setJMenuBar(menuBar);
+
+        // Add action listener to toggle theme
+        toggleTheme.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (mainPanel.getBackground().equals(Color.BLACK)) {
+                    mainPanel.setBackground(Color.WHITE);
+                    buttonPanel.setBackground(Color.WHITE);
+                    imagePanel.setBackground(Color.WHITE);
+                    frame.getContentPane().setBackground(Color.WHITE);
+                } else {
+                    mainPanel.setBackground(Color.BLACK);
+                    buttonPanel.setBackground(Color.BLACK);
+                    imagePanel.setBackground(Color.BLACK);
+                    frame.getContentPane().setBackground(Color.BLACK);
+                }
+            }
+        });
 
         frame.add(mainPanel);
         frame.pack();
