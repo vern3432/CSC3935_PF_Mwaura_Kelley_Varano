5ac2da443442ceed365d31fe2654b0574af2f2d0
Aidan <varanoab@merrimack.edu>
2024-04-29 19:13:05 -0400

    JSON objects for library Storage and creation

--- rtsp/src/main/java/rtspmedia/Server/LibraryMangement/Album.java
+++ rtsp/src/main/java/rtspmedia/Server/LibraryMangement/Album.java
@@ -1,30 +1,61 @@
 package rtspmedia.Server.LibraryMangement;
 
 import java.io.InvalidObjectException;
+import java.util.ArrayList;
 
 import merrimackutil.json.JSONSerializable;
+import merrimackutil.json.JsonIO;
 import merrimackutil.json.types.JSONType;
+import merrimackutil.json.types.JSONArray;
+import merrimackutil.json.types.JSONObject;
 
-public class Album implements JSONSerializable{
+public class Album implements JSONSerializable {
+    private ArrayList<Song> songs = new ArrayList<>();
+
+    public Album() {
+    }
+
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
 
     @Override
-    public void deserialize(JSONType arg0) throws InvalidObjectException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deserialize'");
+    public void deserialize(JSONType json) throws InvalidObjectException {
+        if (!(json instanceof JSONObject))
+            throw new InvalidObjectException("JSON object expected");
+
+        Object jsonSongsObject = ((JSONObject) json).get("songs");
+        if (jsonSongsObject instanceof JSONArray) {
+            JSONArray jsonSongs = (JSONArray) jsonSongsObject;
+            for (int i = 0; i < jsonSongs.size(); i++) {
+                JSONObject jsonSong = (JSONObject) jsonSongs.get(i);
+                Song song = new Song(
+                    jsonSong.getString("name"),
+                    jsonSong.getString("albumImage"),
+                    jsonSong.getString("path")
+                );
+                songs.add(song);
+            }
+        }
     }
 
     @Override
     public String serialize() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'serialize'");
+        return toJSONType().toJSON();
     }
 
     @Override
     public JSONType toJSONType() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toJSONType'");
+        JSONObject jsonObj = new JSONObject();
+        JSONArray jsonSongs = new JSONArray();
+        for (Song song : songs) {
+            jsonSongs.add(song.toJSONType());
+        }
+        jsonObj.put("songs", jsonSongs);
+        return jsonObj;
     }
-
-
-
 }
--- rtsp/src/main/java/rtspmedia/Server/LibraryMangement/Song.java
+++ rtsp/src/main/java/rtspmedia/Server/LibraryMangement/Song.java
@@ -1,28 +1,45 @@
 package rtspmedia.Server.LibraryMangement;
 
 import java.io.InvalidObjectException;
+import java.util.Base64;
 
 import merrimackutil.json.JSONSerializable;
 import merrimackutil.json.types.JSONType;
+import merrimackutil.json.types.JSONObject;
 
-public class Song implements JSONSerializable{
+public class Song implements JSONSerializable {
+    private String name;
+    private String albumImage; // Base64 encoded image
+    private String path; // Absolute path to the song file
+
+    public Song(String name, String albumImage, String path) {
+        this.name = name;
+        this.albumImage = albumImage;
+        this.path = path;
+    }
 
     @Override
-    public void deserialize(JSONType arg0) throws InvalidObjectException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deserialize'");
+    public void deserialize(JSONType json) throws InvalidObjectException {
+        if (!(json instanceof JSONObject))
+            throw new InvalidObjectException("JSON object expected");
+
+        JSONObject jsonObj = (JSONObject) json;
+        this.name = jsonObj.getString("name");
+        this.albumImage = jsonObj.getString("albumImage");
+        this.path = jsonObj.getString("path");
     }
 
     @Override
     public String serialize() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'serialize'");
+        return toJSONType().toJSON();
     }
 
     @Override
     public JSONType toJSONType() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toJSONType'");
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("name", name);
+        jsonObj.put("albumImage", albumImage);
+        jsonObj.put("path", path);
+        return jsonObj;
     }
-    
 }
